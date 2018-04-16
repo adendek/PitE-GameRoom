4dd3a6d3a3ffdd213344357d350c8626661ac4f4
Mario <carricato.mario@gmail.com>
2018-04-16 19:41:44 +0200

    Add ClientGuessNumber

---
+++ Clients/ClientGuessNumber.py
@@ -0,0 +1,68 @@
+#ClientTicTacToe
+#Authors Mario Carricato & Marco Amato
+
+import socket
+import json
+import os
+import time
+
+
+class ClientGuessNumber:
+    def __init__(self):
+        self.client_socket = None
+
+    def set_client_socket(self, client_socket):
+        self.client_socket = client_socket
+
+    def start_client(self):
+
+        welcome_message = self.client_socket.recv(1024)
+        print(welcome_message.decode())
+
+        is_game_ended = False
+
+        while not is_game_ended:
+
+            response = self.client_socket.recv(1024)
+
+            try:
+                json_object = json.loads(response.decode())
+                type_message = json_object.get("type")
+
+                if type_message == 0:
+
+                       while is_choice_wrong:
+
+                        print("\n")
+                        input_choice = input(message)
+                        if input_choice.isdigit():
+                            choice = int(input_choice)
+                            if choice < 101:
+                                is_choice_wrong = False
+
+                                self.client_socket.send(str(choice).encode())
+
+                            else:
+                                print("Invalid input")
+                                time.sleep(0.5)
+
+                        else:
+                            print("Invalid input")
+                            time.sleep(0.5)
+
+                elif type_message == 1:
+
+                    os.system("clear")
+                    message = json_object.get("message")
+                    print("\n"+message)
+
+                else:
+
+                    os.system("clear")
+                    message = json_object.get("message")
+                    print("\n" + message)
+                    is_game_ended = True
+
+            except:
+                print('bad json')
+                break
